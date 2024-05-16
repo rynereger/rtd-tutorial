e750b2cfc72968828a72dfffb8c0ee3227d94a75
rynereger <125768441+rynereger@users.noreply.github.com>
2024-05-15 23:12:54 -0700

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'NNSST'
+copyright = '2024, Yazzie and Reger'
+author = 'Yazzie and Reger'
 
 release = '0.1'
 version = '0.1.0'
